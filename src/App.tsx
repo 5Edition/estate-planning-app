f363fd10d138048a2d95d3e9c53c228960868d61
Dave Kirkland <27985178+5Edition@users.noreply.github.com>
2026-06-22 18:24:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
-export default function App() {
+function App() {
   return (
     <div>
       <h1>Estate Planning Portal</h1>
-      <p>Welcome to your secure estate management dashboard.</p>
     </div>
   );
 }
+
+export default App;
