94f4892384df95ea13a5a5bbb846309127b1a954
Dave Kirkland <27985178+5Edition@users.noreply.github.com>
2026-06-22 09:39:12 -0500

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Estate Planning Portal</h1>
+      <p>Welcome to your secure estate management dashboard.</p>
+    </div>
+  );
+}
